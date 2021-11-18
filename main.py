5f850aef615e60570cbd79f2353392662d78e3c3
Rafał Opalanko <rafalopalanko@gmail.com>
2021-11-18 18:45:58 +0200

    changed place of date lists

--- main.py
+++ main.py
@@ -209,9 +209,12 @@ class SettingScreen(Screen):
 
     def range_clicked(self, range):
         pass
-        
+
 
 class Motivator(App):
+    years_list = ["2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"]
+    months_list = ["Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru"]
+    days_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
